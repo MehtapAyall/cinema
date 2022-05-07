9cf882b2a51384b36edae4aebc1e28213f74ee4b
MehtapAyall <mehtapayal60@gmail.com>
2022-05-07 20:37:19 +0300

    mail atma

--- resources/views/koltuk.blade.php
+++ resources/views/koltuk.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Filmler</title>
 </head>
 
 </html><link rel="stylesheet" href="{{ asset('css/a.css') }}" type="text/css">
@@ -107,7 +107,7 @@
 
   </table><br>
   
-  <a href="bilet/{{$bilet->biletid}}"><button name="btn_giris" class="btn btn-block btn-primary "  >BİLET AL</button></a>
+  <button name="btn_giris" class="btn btn-block btn-primary "  >BİLET AL</button>
 
 
 </form>
